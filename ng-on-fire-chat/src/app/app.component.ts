13dd00fbfb6bd7aeb42d4624b4b5952c19c436f3
kazuki19992 <kushida98@gmail.com>
2020-02-15 21:17:22 +0900

    とりあえずUnknownにはできた

--- ng-on-fire-chat/src/app/app.component.ts
+++ ng-on-fire-chat/src/app/app.component.ts
@@ -3,41 +3,53 @@ import { AngularFirestore } from '@angular/fire/firestore'; // 追加
 import { Observable } from 'rxjs'; // 追加
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-on-fire-chat';
+    title = 'ng-on-fire-chat';
 
-  messages: Observable<any[]>;
-  // inputMessage = 'リアルタイムデータバインド';
-  // inputName = 'Unknown';
-  inputMessage;
-  inputName;
-  constructor(
-    private db: AngularFirestore // DI
-  ) {
-    // idFieldを追加すると、そのドキュメントのインデックスが、指定した名前のプロパティに追加される
-    this.messages = db.collection('messages', ref => ref.orderBy('createdAt', 'desc')).valueChanges({ idField: 'id' }); // DBと接続
+    messages: Observable<any[]>;
+    // inputMessage = 'リアルタイムデータバインド';
+    // inputName = 'Unknown';
+    inputMessage;
+    inputName;
+    constructor(
+        private db: AngularFirestore // DI
+    ) {
+        // idFieldを追加すると、そのドキュメントのインデックスが、指定した名前のプロパティに追加される
+        this.messages = db.collection('messages', ref => ref.orderBy('createdAt', 'desc')).valueChanges({ idField: 'id' }); // DBと接続
 
-    // console.log(this.messages);
-  }
+        // console.log(this.messages);
+    }
 
-  // 送信処理
-  sendMessage() {
-    // console.log('動いてるよ!');
-    this.db.collection('messages').add({
+    // 送信処理
+    sendMessage() {
+        // console.log('動いてるよ!');
 
-        name: this.inputName,
-        body: this.inputMessage,
-        createdAt: new Date() // ここを追加
-    });
-  }
+        // メッセージが存在しない場合は投稿しない
+        if (typeof(this.inputMessage) !== 'undefined' && this.inputMessage.length !== 0){
+            // ユーザー名が空欄ならば'Unknown'に置換え
+            if (typeof(this.inputName) === 'undefined' || this.inputName.length === 0) {
+                this.db.collection('messages').add({
+                    name: 'Unknown',
+                    body: this.inputMessage,
+                    createdAt: new Date() // ここを追加
+                });
+            } else {
+                this.db.collection('messages').add({
+                        name: this.inputName,
+                        body: this.inputMessage,
+                        createdAt: new Date() // ここを追加
+                });
+            }
+        }
+    }
 
-  // 削除処理
-  // 削除ボタンが押されたメッセージを削除する
-  deleteMessage(message) {
-    this.db.collection('messages').doc(message.id).delete();
-  }
+    // 削除処理
+    // 削除ボタンが押されたメッセージを削除する
+    deleteMessage(message) {
+        this.db.collection('messages').doc(message.id).delete();
+    }
 }
